6075d40c05164100b9bbd070642738ade46fc706
Rayane Reis <57107726+Yamurin@users.noreply.github.com>
2024-02-20 11:57:53 -0300

    Update hanoi.c
    Todo: Fix printMenu and related function calls in main()
    Todo: Improve user input handling, remove it from main() to a sepaparte function

--- alg-prog-2/hanoi/hanoi.c
+++ alg-prog-2/hanoi/hanoi.c
@@ -1,68 +1,125 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TORRES 3 
 #define BLOCOS 5
+
 enum Torres { a, b, c };
+int mat[BLOCOS][TORRES] = {0};
+
+void printMatriz (void)
+{
+    for (int i = 0; i < BLOCOS; i++)
+        {
+            for (int j = 0; j < TORRES; j++)
+                printf("%d ", mat[i][j]);
+            printf("\n");
+        }
+}
+void printMenu (void)
+{
+    printf("\nEscolha a próxima jogada\n");
+    
+    printf(" 1 ) Mover de A para B\n");
+    printf(" 2 ) Mover de A para C\n");
+    printf(" 3 ) Mover de B para C\n");
+    printf(" 4 ) Mover de B para A\n");
+    printf(" 5 ) Mover de C para A\n");
+    printf(" 6 ) Mover de C para B\n");
+    
+    printf(" 7 ) Desistir do jogo\n");
+    
+    printf(" -> ");
+}
+
+// Transpõe um bloco da pilha de origem para a pilha de destino, informados pelo usuário
+void moverBloco(int orig, int dest, int *ptrOr, int *ptrDs)
+{
+    // Define o bloco atualmente no topo da pilha de origem
+    int i = 0;
+    while(mat[i][orig] == 0)
+        i++;
+    ptrOr = &mat[i][orig];
+    
+     // Define o bloco atualmente no topo da pilha de destino
+    i = BLOCOS - 1;
+    while(mat[i][dest] != 0)
+        i--;
+    ptrDs = &mat[i][dest];
+    
+    // TODO Erros Handling
+    /*if (*ptrOr >=2 && *ptrOr > *ptrDs)
+    {
+        printf("MOVIMENTO INVÁLIDO: Não é permitido mover um bloco maior acima de um bloco de valor menor.\n");
+        return;
+    }*/
+    
+    // Mover bloco da origem para o destino
+    *ptrDs = *ptrOr;
+    *ptrOr = 0;  
+}
 
 int main()
 {
     printf("Iniciando jogo \n");
-    int mat[BLOCOS][TORRES] = {0};
     
-     // Popular o tabuleiro inicial
+     // Preencher o tabuleiro inicial
     for(int i = 0; i < BLOCOS; i++)
         mat[i][a] = i + 1;
-    
-    /*
-        1 0 0
-        2 0 0
-        3 0 0
-        4 0 0
-        5 0 0
-    */
-    
+        
     int *pa = &mat[0][a];
     int *pb = NULL;
     int *pc = NULL;
-    int i, temp;
+    int userEscolha;
     
-    while (1)
+    while(mat[0][c] != 5)
     {
-        printf("Opcao: \n");
-        printf("1.A para B \n2.A para C\n3.B para A\n4.B para C\n");
-        scanf("%d", &i);
+        printMatriz();
         
-        switch (i)
+        printMenu();
+        scanf("%d", &userEscolha);
+        
+        switch (userEscolha)
         {
-            case 1:
-                
-                // como acessar pb?
-                *pb = *pa;
-                temp = *pa; 
-                *pa = 0;
-                pa = &mat[temp][a];
+            case (1):
+                moverBloco(a, b, pa, pb);
                 break;
-                
-            case 2:
-                temp = BLOCOS - (*pa);
-                pc = &mat[temp][c];
-                *pc = *pa;
-                temp = *pa;  // Calcula qual o indice do ultimo bloco, depois de tirar o de cima
-                *pa = 0;
-                pa = &mat[temp][a];
+            case (2):
+                moverBloco(a, c, pa, pc);
                 break;
+            case (3):
+                moverBloco(b, a, pb, pa);
+                break;
+            case (4):
+                moverBloco(b, c, pb, pc);
+                break;
+            case (5):
+                moverBloco(c, a, pc, pa);
+                break;
+            case (6):
+                moverBloco(c, b, pc, pb);
+                break;
+            case (7):
+                exit(1);
         }
         
-         for(int i = 0; i < BLOCOS; i++)
-         {
-             for(int j = 0; j < TORRES; j++)
-                printf("%d ", mat[i][j]);
-            printf("\n");
-         }  
+        printMatriz();
+        printf("\n");
     }
     
-    
+    // EXEMPLO: Mover de A para C 
+    /*while(mat[0][b] != 5)
+    {
+        mover_bloco(a, b, pa, pc);
+        
+        for (int i = 0; i < BLOCOS; i++)
+        {
+            for (int j = 0; j < TORRES; j++)
+                printf("%d ", mat[i][j]);
+            printf("\n");
+        }
+        
+        printf("\n");
+    }*/
     return 0;
-    
 }
- 
