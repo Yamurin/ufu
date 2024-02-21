815ab0dad2b67c5becd6798a74ab925e548a1bd9
Rayane Reis <57107726+Yamurin@users.noreply.github.com>
2024-02-21 16:23:28 -0300

    Update hanoi.c
    EVERYTHING works, except pointing to null when a rod get empty

--- alg-prog-2/hanoi/hanoi.c
+++ alg-prog-2/hanoi/hanoi.c
@@ -2,64 +2,75 @@
 #include <stdlib.h>
 
 #define TORRES 3 
-#define BLOCOS 5
+#define BLOCOS 3
 
 enum Torres { a, b, c };
 int mat[BLOCOS][TORRES] = {0};
 
+// Mensagem 
+void printContinuar (void)
+{
+    printf("\nContinuar...");
+    setbuf(stdin, NULL);
+    getchar();
+}
+
+// Apresenta toda a matriz na tela.
 void printMatriz (void)
 {
+    printf("\nA\tB\tC\n------------------\n");
     for (int i = 0; i < BLOCOS; i++)
         {
             for (int j = 0; j < TORRES; j++)
-                printf("%d ", mat[i][j]);
+                printf("%d \t", mat[i][j]);
             printf("\n");
         }
-        
-    getchar();
-    setbuf(stdin, NULL);
-    
+    printContinuar();
     return; 
 }
+
+// Apresenta as opções na tela.
 void printMenu (void)
 {
     printf("\nEscolha a próxima jogada\n");
     
-    printf(" 1 ) Mover de A para B\n");
-    printf(" 2 ) Mover de A para C\n");
-    printf(" 3 ) Mover de B para A\n");
-    printf(" 4 ) Mover de B para C\n");
-    printf(" 5 ) Mover de C para A\n");
-    printf(" 6 ) Mover de C para B\n");
-    
+    printf(" 1 ) Mover A > B \t\t 2 ) Mover A > C\n");
+    printf(" 3 ) Mover B > A \t\t 4 ) Mover B > C\n");
+    printf(" 5 ) Mover C > A \t\t 6 ) Mover C > B\n");
     printf(" 7 ) Desistir do jogo\n");
-    
     printf(" -> ");
 }
 
 void moveBloco(int orig, int dest, int **ptrOr, int **ptrDs)
 {
-    // Vê se aposição é válida
-    if (*ptrDs != NULL && **ptrOr > **ptrDs)
-    {
-        printf("INVALIDO\n");
+    // todo paontar pra null quando vazio
+    // Verifica se o movimento é válido.
+    if (*ptrOr == NULL) {
+        printf("MOVIMENTO INVALIDO: Voce nao pode mover blocos de uma torre vazia.\n");
+        printContinuar();
+        return;
+    } else if (*ptrDs != NULL && **ptrDs != 0 && **ptrOr > **ptrDs) {
+        printf("orig %d : dest %d\n", **ptrOr, **ptrDs);
+        printf("MOVIMENTO INVALIDO: Voce nao pode colocar um bloco em cima de outro com diametro menor. Tente novamente.\n");
+        printContinuar();
         return;
     }
     
-    // Índices auxiliares
     int i = 0;
     int j = BLOCOS - 1;
      
-    // Define o bloco atualmente no topo da pilha de origem
-    while(mat[j][dest] != 0) { j--;}
+    // Define o bloco atualmente no topo da pilha de origem.
+    while(mat[j][dest] != 0) 
+        j--;
     *ptrDs = &mat[j][dest];
     
-    // Move o bloco da origem para o destino. Reseta o bloco de origem
+    // Move o bloco da origem para o destino. Reseta o bloco de origem.
     **ptrDs = **ptrOr;
     **ptrOr = 0;
     
-    // Define o bloco atualmente no topo da pilha de destino
-    while(mat[i][orig] == 0 && i < BLOCOS) { i++; }
+    // Define o bloco atualmente no topo da pilha de destino.
+    while(mat[i][orig] == 0 && i <= BLOCOS) 
+        i++; 
     *ptrOr = &mat[i][orig];
     
     return; 
@@ -67,7 +78,7 @@ void moveBloco(int orig, int dest, int **ptrOr, int **ptrDs)
             
 int main()
 {
-    printf("Iniciando jogo \n");
+    printf("Iniciando jogo...\n");
     
      // Preencher o tabuleiro inicial
     for(int i = 0; i < BLOCOS; i++)
@@ -78,11 +89,12 @@ int main()
     int *pc = NULL;
     int userEscolha ;
    
-    while(mat[0][c] != 5)
+    while(mat[0][c] != 1)
     {
         printMatriz();
         printMenu();
         scanf("%d", &userEscolha);
+        
         switch (userEscolha)
         {
             case (1): // A > B
@@ -95,7 +107,6 @@ int main()
                 
             case (3): // B > A
                 moveBloco(b, a, &pb, &pa);
-
                 break;
                 
             case (4): // B > C
@@ -111,8 +122,12 @@ int main()
                 break;
                 
             case (7):
+                printf("\nSaindo do jogo...\n");
+                getchar();
                 exit(1);
         }
     }
+    
+    printf("Parabens! Voce ganhou!\n");
     return 0;
 }
